50e3831fc80f19a04b8766a7ac81f51caac6a8f0
Jimmy Jimenez <jimmy.jimenez@jjay.cuny.edu>
2024-02-13 09:01:41 -0500

    recursively call fibonacci_recursive function that will return array based on user input

--- Fibonacci/Fibonacci_recursive.js
+++ Fibonacci/Fibonacci_recursive.js
@@ -1,5 +1,24 @@
-function fibonacci_recursive(numberInput) {
-  if (numberInput < 2) {
-    return [0, 1];
+function fibonacci_recursive(numberInput, cage = [0, 1]) {
+  if (numberInput <= 2) {
+    return cage;
   }
+
+  let previousFirst = cage[cage.length - 1];
+  let previousSecond = cage[cage.length - 2];
+
+  let sum = previousFirst + previousSecond;
+
+
+  cage.push(sum);
+
+  return fibonacci_recursive(numberInput - 1, cage);
+
+
 }
+
+
+let e = fibonacci_recursive(6);
+
+console.log(e)
+
+
